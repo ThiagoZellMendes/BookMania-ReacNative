430d69b34f6a652d12a87e3b9f71aa5c9abbeb70
Thiago Mendes <thiagosmendes.th@gmail.com>
2021-12-26 15:51:34 -0300

    Navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import Router from './routes/router';
+import theme from './src/global/theme';
 
-export default function App() {
+
+export default function App(){
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    <ThemeProvider theme={theme}>
+      <StatusBar  backgroundColor="#FFFFFF" translucent={true} barStyle='dark-content' />
+      <Router />  
+    </ThemeProvider>
+  );}
---
+++ src/global/theme.ts
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    backgroundColor:'#FFFFFF',
+  
+    primary:'#333333',
+
+    gray3: '#828282',
+
+    CBiografia: '#F2994A',
+    CTecnologia: '#219653',
+    CFiccao: '#9B51E0',
+    CTPolitica: '#4F4F4F',
+    CAutoAjuda: '#56CCF2',
+    Border: '#BDBDBD'
+  },
+  
+  fonts: {
+    regular: 'lato'
+  }
+}
