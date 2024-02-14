7c7f125eeed235421630b5f851b1aa4d3063d17b
Thiago Mendes <thiagosmendes.th@gmail.com>
2024-02-13 22:42:59 -0300

    Feat: Create-abstration-books-flow

---
+++ src/domain/models/books/IBookDescription.ts
@@ -0,0 +1,12 @@
+export interface IBookDescription {
+  title: string;
+  author: string;
+  star: number;
+  book_image: string;
+  description: string;
+  publisher: string;
+  buy_links: {
+    name: string;
+    url: string;
+  }
+}
